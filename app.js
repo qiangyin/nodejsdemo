4c9834c60ee18c08d405e87aa1c32e5488b4f110
yintongqiang <yintongqiang@cd.tuan800.com>
2014-08-12 17:01:04 +0800

    bootstrap2换到bootstrap3.2

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(function(req, res, next){
 });
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
